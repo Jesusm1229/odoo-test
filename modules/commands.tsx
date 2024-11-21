2f3e95e245d7a8d5f3b0519225a7b02759fb58af
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2024-11-21 07:02:27 -0400

    Moving command txt file

--- modules/commands.tsx
+++
@@ -1,5 +0,0 @@
-pip install -r requirements.txt
-
-python odoo-bin -r admin -w 1230 --addons-path=addons,modules -d odoo
-
-python odoo-bin -r admin -w 1230 --addons-path=addons,modules -d odoo -u school
